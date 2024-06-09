c3518e7994441010c65668db1750176aa9635374
brunoSacchetti <sacchettibruno03@gmail.com>
2024-06-09 19:07:37 -0300

    arreglando back

---
+++ src/main/java/com/entidades/buenSabor/presentation/rest/ArticuloInsumoController.java
@@ -0,0 +1,103 @@
+package com.entidades.buenSabor.presentation.rest;
+
+import com.entidades.buenSabor.business.facade.Imp.ArticuloInsumoFacadeImp;
+import com.entidades.buenSabor.business.service.ArticuloInsumoService;
+import com.entidades.buenSabor.domain.dto.Insumo.ArticuloInsumoCreateDto;
+import com.entidades.buenSabor.domain.dto.Insumo.ArticuloInsumoDto;
+import com.entidades.buenSabor.domain.dto.Insumo.ArticuloInsumoEditDto;
+import com.entidades.buenSabor.domain.entities.ArticuloInsumo;
+import com.entidades.buenSabor.presentation.rest.Base.BaseControllerImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/ArticuloInsumo")
+public class ArticuloInsumoController  extends BaseControllerImp<ArticuloInsumo, ArticuloInsumoDto, ArticuloInsumoCreateDto, ArticuloInsumoEditDto, Long, ArticuloInsumoFacadeImp> {
+    public ArticuloInsumoController(ArticuloInsumoFacadeImp facade) {
+        super(facade);
+    }
+
+    @Autowired
+    ArticuloInsumoService articuloInsumoService;
+
+    @PutMapping("/changeHabilitado/{id}")
+    public ResponseEntity<?> changeHabilitado(@PathVariable Long id){
+        facade.changeHabilitado(id);
+        return ResponseEntity.ok().body("Se cambio el estado del Insuomo");
+    }
+
+    @GetMapping("/getHabilitados")
+    public ResponseEntity<?> getHabilitados(){
+        return ResponseEntity.ok(facade.getAllHabilitados());
+    }
+
+
+    // Método POST para subir imágenes
+    /* @PostMapping("/uploads")
+    public ResponseEntity<String> uploadImages(
+            @RequestParam(value = "uploads", required = true) MultipartFile[] files,
+            @RequestParam(value = "id", required = true) Long idArticulo) {
+        try {
+            return facade.uploadImages(files, idArticulo); // Llama al método del servicio para subir imágenes
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null; // Manejo básico de errores, se puede mejorar para devolver una respuesta más específica
+        }
+    } */
+
+    @PostMapping("/uploads")
+    public ResponseEntity<String> uploadImages(@RequestParam("uploads") MultipartFile[] files,
+                                               @RequestParam("id") Long idArticulo) {
+        return articuloInsumoService.uploadImages(files, idArticulo);
+    }
+
+    @DeleteMapping("/{publicId}/imagenes/{id}")
+    public ResponseEntity<String> deleteImage(@PathVariable String publicId, @PathVariable Long id) {
+        return articuloInsumoService.deleteImage(publicId, id);
+    }
+
+    @GetMapping("/getImagesByArticuloId/{id}")
+    public ResponseEntity<?> getAll(@PathVariable Long id) {
+        try {
+            return articuloInsumoService.getAllImagesByArticuloId(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
+    }
+
+    // Método POST para eliminar imágenes por su publicId y Long
+    /* @PostMapping("/deleteImg")
+    public ResponseEntity<String> deleteById(
+            @RequestParam(value = "publicId", required = true) String publicId,
+            @RequestParam(value = "id", required = true) Long id) {
+        try {
+            return facade.deleteImage(publicId, id); // Llama al método del servicio para eliminar la imagen
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("Invalid UUID format"); // Respuesta HTTP 400 si el UUID no es válido
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("An error occurred"); // Respuesta HTTP 500 si ocurre un error inesperado
+        }
+    } */
+
+    // Método GET para obtener todas las imágenes almacenadas
+    /* @GetMapping("/getImagesByArticuloId/{id}")
+    public ResponseEntity<?> getAll(@PathVariable Long id) {
+        try {
+            return facade.getAllImagesByArticuloId(id); // Llama al método del servicio para obtener todas las imágenes
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null; // Manejo básico de errores, se puede mejorar para devolver una respuesta más específica
+        }
+    } */
+}
